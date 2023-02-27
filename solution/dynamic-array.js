ccde02232add542e0de7768e91b719975d4856cc
Nick <nick.buscemi13@gmail.com>
2023-02-27 15:59:43 -0500

    FINISHED

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -1,19 +1,35 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
+    
 
   }
 
   read(index) {
-
-    // Your code here
+    if (index < 0 || index >= this.length){
+      return undefined;
+    }
+    return this.data[index];
   }
 
   unshift(val) {
-
-    // Your code here
+    if (this.length >= this.capacity){
+      const newData = new Array(this.capacity * 2);
+      for (let i = 0; i < this.length; i++) {
+        newData[i + 1] = this.data[i];
+      }
+      this.data = newData;
+      this.capacity *= 2;
+      } else {
+        for (let i = this.length; i > 0; i--) {
+          this.data[i] = this.data[i - 1];
+        }
+      }
+      this.data[0] = val;
+      this.length++;
   }
 
 }
